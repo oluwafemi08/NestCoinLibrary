7ce2ede2cc0483d299ffce2c73ef82163cd09cc9
Banji Femi <banjioluwafemi22@gmail.com>
2022-04-15 09:52:19 +1000

    for the comments

--- contracts/NestcoinLibrary.sol
+++ contracts/NestcoinLibrary.sol
@@ -1,75 +1,125 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.5;
 
 contract Filestorage {
     string public name;
-    uint countPublic = 0;
-    uint countPrivate = 0;
-    
-    enum Status { Public, Private }
-    
+    uint256 countPublic = 0;
+    uint256 countPrivate = 0;
+
+    enum Status {
+        Public,
+        Private
+    }
+
+    //struct for public file
     struct Publicfile {
-        uint count;
+        uint256 count;
         address owner;
         string ipfsHash;
         Status status;
     }
-     struct Privatefile {
-        uint count;
+
+    //struct for private file
+    struct Privatefile {
+        uint256 count;
         string ipfsHash;
         Status status;
     }
-
-    struct sharedFile {
+    //struct for shared file
+    struct SharedFile {
         address shared_by;
         string shared_hash;
     }
+    //mapping of public files
+    mapping(uint256 => Publicfile) public publicfiles;
+    //mapping of internal private files
+    mapping(uint256 => Privatefile) internal privatefiles; //address
+    mapping(address => SharedFile) public shared_files;
 
-    mapping (uint => Publicfile) public publicfiles;
-    mapping (address => Privatefile[]) public privatefiles;
-    mapping (address => sharedFile[]) public shared_files;
+    //an event for when a file is created by the owner
+    event FileCreation(
+        uint256 count,
+        address owner,
+        string Hash,
+        Status status
+    );
 
-    event FileCreation ( uint count, address owner, string Hash, Status status);
-
-    constructor (string memory _name) {
+    //the FileStorage name is created using the constructor
+    constructor(string memory _name) {
         name = _name;
     }
 
-    function addFile ( string memory _hash, string memory _status ) public {
-      if(keccak256(abi.encodePacked(_status)) == keccak256(abi.encodePacked("public"))) {
-            countPublic ++;
-            publicfiles[countPublic] = Publicfile( countPublic, msg.sender, _hash, Status.Public);
-            emit FileCreation(countPublic, msg.sender, _hash, Status.Public );
+    //add files to FileStorage
+    function addFile(string memory _hash, string memory _status) public {
+        //check if status of file added is Public.
+        if (
+            keccak256(abi.encodePacked(_status)) ==
+            keccak256(abi.encodePacked("public"))
+        ) {
+            //increment countPublic
+            countPublic++;
+            publicfiles[countPublic] = Publicfile(
+                countPublic,
+                msg.sender,
+                _hash,
+                Status.Public
+            );
+            //emit event FileCreation has been added to publicfile
+            emit FileCreation(countPublic, msg.sender, _hash, Status.Public);
+        } else {
+            //increment countPrivate
+            countPrivate++;
+            privatefiles[countPrivate] = (
+                Privatefile(countPrivate, _hash, Status.Private)
+            );
+            //emit event FileCreation has been added to privatefile
+            emit FileCreation(countPrivate, msg.sender, _hash, Status.Private);
         }
-    else {
-        countPrivate ++;
-        privatefiles[msg.sender].push(Privatefile(countPrivate, _hash, Status.Private ));
-        emit FileCreation(countPrivate, msg.sender, _hash, Status.Private );
     }
-    }
-    
-    // Retrieve files
-    function retrievePublicFile () public view returns(Publicfile[] memory) {
-      Publicfile[] memory public_files = new Publicfile[] (countPublic);
-      for(uint i=0; i<countPublic; i++) {
-        Publicfile storage public_file = publicfiles[i];
+
+    // Retrieve public files
+    function retrievePublicFile() public view returns (Publicfile[] memory) {
+        Publicfile[] memory public_files = new Publicfile[](countPublic);
+        for (uint256 i = 0; i < countPublic; i++) {
+            Publicfile storage public_file = publicfiles[i];
             public_files[i] = public_file;
-      }
-      return public_files;
-  }
+        }
+        return public_files;
+    }
 
-  function retrievePrivateFile (address _address) public view returns (Privatefile[] memory) {
-      require (address = msg.sender, "You do not have Access to retrieve this file");
-      return privatefiles[msg.sender];
-  }
+    //retrieve private file
+    function retrievePrivateFile(address _address)
+        public
+        view
+        returns (Privatefile[] memory)
+    {
+        require(
+            _address == msg.sender,
+            "You do not have Access to retrieve this file"
+        );
 
-  function shareFile ( address _share_to, string memory _hashed_file ) public {
-      require( _share_to != address(0), "Actual address is required");
-      shared_files[_share_to].push(sharedFile(msg.sender, _hashed_file));
-  }
+        Privatefile[] memory private_files = new Privatefile[](countPrivate);
+        for (uint256 i = 0; i < countPrivate; i++) {
+            Privatefile storage private_file = privatefiles[i];
+            private_files[i] = private_file;
+        }
+        return private_files; //[msg.sender]
+    }
 
-  function getSharedFile(address _address) public returns (sharedFile[] memory) {
-      return shared_files[msg.sender];
-  }
+    //share files to address not owned by the account(0)
+    function shareFile(address _addressTo, string memory _hashed_file) public {
+        require(_addressTo != address(0), "Actual address is required");
+        require(_addressTo != msg.sender, "Actual address is required");
+        shared_files.push(SharedFile(msg.sender, _hashed_file));
+        //_share_to                     //_share_to
+    }
+
+    //fectch shared files
+    function getSharedFile(address _address)
+        public
+        returns (SharedFile[] memory)
+    {
+        return shared_files[msg.sender];
+    }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,11 @@ require("@nomiclabs/hardhat-ethers");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+   for (const account of accounts) {
+      console.log(account.address);
+   }
 });
 
 // You need to export an object to set up your config
@@ -23,12 +23,14 @@ const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.5",
-   defaultNetwork: "rinkeby",
+   // defaultNetwork: "rinkeby",
    networks: {
-      hardhat: {},
-      rinkeby: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
-      }
+      hardhat: {
+         chainID: 1337
+      },
+      // rinkeby: {
+      //    url: API_URL,
+      //    accounts: [`0x${PRIVATE_KEY}`]
+      // }
    },
 }
